5215c3d2d8ab002bbd59f30e3ead412130fbfa52
tderamus <deramust@gmail.com>
2025-01-27 22:28:03 -0600

    adding basic models, views and controllers to this project

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel dog = new DogViewModel
+            {
+                Name = "Fido",
+                Breed = "Golden Retriever",
+                Age = 5
+            };
+            return View(dog);
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
